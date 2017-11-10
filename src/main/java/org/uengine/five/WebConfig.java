29e738482c6df7e50c160cfde956f9cdba9e9191
jinyoung jang <jyjang@uengine.org>
2017-11-10 17:31:44 +0900

    no message

--- src/main/java/org/uengine/five/WebConfig.java
+++ src/main/java/org/uengine/five/WebConfig.java
@@ -13,7 +13,9 @@ import org.uengine.social.repository.ProcessInstanceRepository;
 import org.uengine.social.service.DefinitionService;
 
 import javax.servlet.Filter;
+import javax.sql.DataSource;
 import java.util.Map;
+import java.util.Properties;
 
 @EnableWebMvc
 @Configuration
@@ -59,7 +61,25 @@ public class WebConfig extends org.uengine.social.uEngine5WebConfig {
 //        return new CustomProcessInstance(procDefinition, instanceId, options);
 //    }
 
+    /**
+     * Uncomment and return an real datasource not the H2
+     */
 
+//    @Bean
+//    public DataSource dataSource() {
+//        //In classpath from spring-boot-starter-web
+//        final Properties pool = new Properties();
+//        pool.put("driverClassName", "com.mysql.jdbc.Driver");
+//        pool.put("url", "jdbc:mysql://localhost:3306/uengine?useUnicode=true&characterEncoding=UTF8&useOldAliasMetadataBehavior=true");
+//        pool.put("username", "root");
+//        pool.put("password", "");
+//        pool.put("minIdle", 1);
+//        try {
+//            return new org.apache.tomcat.jdbc.pool.DataSourceFactory().createDataSource(pool);
+//        } catch (Exception e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
 
 }
 
