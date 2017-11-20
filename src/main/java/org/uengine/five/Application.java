a3eabedf20807b1fc46a2c59f7fff734c489c4e9
jinyoung jang <jyjang@uengine.org>
2017-11-20 15:31:48 +0900

    Change uEngine5 to be fit in new uEngine5 project structure - multi project

--- src/main/java/org/uengine/five/Application.java
+++ src/main/java/org/uengine/five/Application.java
@@ -1,17 +1,17 @@
 package org.uengine.five;
 
+import org.metaworks.springboot.configuration.Metaworks4BaseApplication;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties;
 import org.springframework.boot.autoconfigure.transaction.TransactionManagerCustomizers;
 import org.springframework.transaction.jta.JtaTransactionManager;
-import org.uengine.social.uEngine5Application;
 
 import javax.sql.DataSource;
 
 @SpringBootApplication
-public class Application extends org.uengine.social.uEngine5Application{
+public class Application extends Metaworks4BaseApplication {
     protected Application(DataSource dataSource, JpaProperties properties, ObjectProvider<JtaTransactionManager> jtaTransactionManagerProvider, ObjectProvider<TransactionManagerCustomizers> transactionManagerCustomizers) {
         super(dataSource, properties, jtaTransactionManagerProvider, transactionManagerCustomizers);
     }
--- src/main/java/org/uengine/five/WebConfig.java
+++ src/main/java/org/uengine/five/WebConfig.java
@@ -1,26 +1,41 @@
 package org.uengine.five;
 
+import org.metaworks.multitenancy.ClassManager;
+import org.metaworks.multitenancy.DefaultMetadataService;
+import org.metaworks.multitenancy.MetadataService;
+import org.metaworks.multitenancy.persistence.MultitenantRepositoryImpl;
+import org.metaworks.multitenancy.tenantawarefilter.TenantAwareFilter;
+import org.metaworks.rest.MetaworksRestService;
 import org.metaworks.springboot.configuration.CorsFilter;
+import org.metaworks.springboot.configuration.Metaworks4WebConfig;
 import org.springframework.context.annotation.*;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.uengine.five.entity.ProcessInstanceEntity;
+import org.uengine.five.overriding.InstanceDataAppendingActivityFilter;
+import org.uengine.five.overriding.JPAProcessInstance;
+import org.uengine.five.overriding.JPAWorkList;
+import org.uengine.five.overriding.ProcessDefinitionFactory;
+import org.uengine.five.repository.ProcessInstanceRepository;
+import org.uengine.five.service.DefinitionServiceImpl;
 import org.uengine.kernel.ActivityFilter;
 import org.uengine.kernel.ProcessDefinition;
 import org.uengine.kernel.ProcessInstance;
 import org.uengine.modeling.resource.CachedResourceManager;
+import org.uengine.modeling.resource.LocalFileStorage;
 import org.uengine.modeling.resource.ResourceManager;
 import org.uengine.modeling.resource.Storage;
-import org.uengine.social.entity.ProcessInstanceEntity;
-import org.uengine.social.repository.ProcessInstanceRepository;
-import org.uengine.social.service.DefinitionService;
+import org.uengine.webservices.worklist.WorkList;
 
 import javax.servlet.Filter;
-import javax.sql.DataSource;
 import java.util.Map;
-import java.util.Properties;
 
 @EnableWebMvc
 @Configuration
-public class WebConfig extends org.uengine.social.uEngine5WebConfig {
+@ComponentScan(basePackageClasses = {DefinitionServiceImpl.class, ProcessDefinitionFactory.class, ProcessInstanceEntity.class, MetaworksRestService.class, ClassManager.class, MetadataService.class, MultitenantRepositoryImpl.class})
+@EnableJpaRepositories(basePackageClasses = {MultitenantRepositoryImpl.class, ProcessInstanceRepository.class})
+public class WebConfig extends Metaworks4WebConfig {
+
 
     /**
      * Uncomment if this needs a CORS setting by itself
@@ -32,64 +47,88 @@ public class WebConfig extends org.uengine.social.uEngine5WebConfig {
     }
 
 
+
+    @Bean
+    public ResourceManager resourceManager() {
+        ResourceManager resourceManager = new CachedResourceManager();
+        resourceManager.setStorage(storage());
+        return resourceManager;
+    }
+
+
+    @Bean
     /**
-     * Uncomment and implement if you want to change default storage
+     *
+     * <bean class="CouchbaseStorage">
+     *    <property name="basePath" value="/"/>
+     <property name="bucketName" value="default"/>
+     <property name="serverIp" value="localhost"/>
+     </bean>
      */
+    public Storage storage() {
+        LocalFileStorage storage = new LocalFileStorage();
+        storage.setBasePath("/oce/repository");
 
-//    @Bean
-//    @Override
-//    public Storage storage() {
-//        CLOBStorage storage = new CLOBStorage();
-//        storage.setDataSource(dataSource());
-//        storage.setTableName("bpm_def");
-//
-//        return storage;
-//    }
+        return storage;
+    }
 
 
-    /**
-     * Uncomment and add ActivityFilters like following example
-     */
-//    @Bean
-//    public ActivityFilter exampleActivityFilter1() {
-//        return new ExampleActivityFilter1();
-//    }
-//
-//    @Bean
-//    public ActivityFilter exampleActivityFilter2() {
-//        return new ExampleActivityFilter2();
-//    }
-//
+    @Bean
+    public MetadataService metadataService() {
+        DefaultMetadataService metadataService = new DefaultMetadataService();
+        metadataService.setResourceManager(resourceManager());
 
-    /**
-     * Uncomment and return an implementation of ProcessInstance. default is JPAProcessInstance
-     */
-//    @Bean
-//    @Scope("prototype")
-//    @Override
-//    public ProcessInstance processInstance(ProcessDefinition procDefinition, String instanceId, Map options) throws Exception {
-//        return new CustomProcessInstance(procDefinition, instanceId, options);
-//    }
+        return metadataService;
+    }
+
+
+    @Bean
+    @Scope("prototype")
+    public ProcessInstance processInstance(ProcessDefinition procDefinition, String instanceId, Map options) throws Exception {
+        return new JPAProcessInstance(procDefinition, instanceId, options);
+    }
+
+    @Bean
+    public ActivityFilter instanceDataAppendingFilter(){
+        return new InstanceDataAppendingActivityFilter();
+    }
+
+    @Bean
+    public WorkList workList() {
+        return new JPAWorkList();
+    }
+
+    @Bean
+    public Filter webFilter() {
+        return new TenantAwareFilter();
+    }
+
+    @Bean
+    public ProcessDefinitionFactory processDefinitionFactory(){
+        return new ProcessDefinitionFactory();
+    }
+
+
 
     /**
      * Uncomment and return an real datasource not the H2
      */
 
-    @Bean
-    public DataSource dataSource() {
-        //In classpath from spring-boot-starter-web
-        final Properties pool = new Properties();
-        pool.put("driverClassName", "com.mysql.jdbc.Driver");
-        pool.put("url", "jdbc:mysql://localhost:3306/uengine?useUnicode=true&characterEncoding=UTF8&useOldAliasMetadataBehavior=true");
-        pool.put("username", "root");
-        pool.put("password", "");
-        pool.put("minIdle", 1);
-        try {
-            return new org.apache.tomcat.jdbc.pool.DataSourceFactory().createDataSource(pool);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
+//    @Bean
+//    public DataSource dataSource() {
+//        //In classpath from spring-boot-starter-web
+//        final Properties pool = new Properties();
+//        pool.put("driverClassName", "com.mysql.jdbc.Driver");
+//        pool.put("url", "jdbc:mysql://localhost:3306/uengine?useUnicode=true&characterEncoding=UTF8&useOldAliasMetadataBehavior=true");
+//        pool.put("username", "root");
+//        pool.put("password", "");
+//        pool.put("minIdle", 1);
+//        try {
+//            return new org.apache.tomcat.jdbc.pool.DataSourceFactory().createDataSource(pool);
+//        } catch (Exception e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
 
 }
 
